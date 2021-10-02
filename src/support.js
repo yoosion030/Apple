f15c9f56a7b669021a6ecdb23edd10b4cd8872da
yoosion <rsy5487@naver.com>
2021-10-02 13:27:13 +0900

    toggle 구현

--- src/support.js
+++ src/support.js
@@ -1,7 +1,22 @@
-const questionBox = document.querySelector('.questionBox');
-const accoridion = document.querySelector('.accordion');
+const questionBox = document.querySelectorAll('.questionBox');
+const accoridion = document.querySelectorAll('.accordion');
 const img = document.querySelector('img');
+const answerBox = document.querySelectorAll('.answerBox');
 
-questionBox.addEventListener('click',()=>{
-    accoridion.
-})
+let haha = 'down';
+
+function answer(i){
+    console.log(i);
+    if(haha === 'down'){
+        haha = 'up';
+        accoridion[i].src = "/images/chevron-up-solid.svg";
+        answerBox[i].style = "display:block";
+    }
+    else {
+        haha = 'down';
+        accoridion[i].src = "/images/chevron-down-solid.svg";
+        answerBox[i].style = "display:none";
+    }
+    
+}
+
