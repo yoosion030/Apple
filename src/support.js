f2c68d22d6f03ed412f425234219b83d457af9a0
yoosion <rsy5487@naver.com>
2021-10-02 10:40:23 +0900

    Add question box

---
+++ src/support.js
@@ -0,0 +1,7 @@
+const questionBox = document.querySelector('.questionBox');
+const accoridion = document.querySelector('.accordion');
+const img = document.querySelector('img');
+
+questionBox.addEventListener('click',()=>{
+    accoridion.
+})
