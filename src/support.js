21538e7cabfcb1f0f5c2b3d1ef3d606e936d17f7
yoosion <rsy5487@naver.com>
2021-10-02 16:15:16 +0900

    toggle 오류 수정

--- src/support.js
+++ src/support.js
@@ -1,22 +1,17 @@
 const questionBox = document.querySelectorAll('.questionBox');
 const accoridion = document.querySelectorAll('.accordion');
-const img = document.querySelector('img');
 const answerBox = document.querySelectorAll('.answerBox');
 
-let haha = 'down';
+let toggleBtn = [0, 0];
 
-function answer(i){
-    console.log(i);
-    if(haha === 'down'){
-        haha = 'up';
+function answer(i) {
+    if (toggleBtn[i] === 0) {
+        toggleBtn[i] = 1;
         accoridion[i].src = "/images/chevron-up-solid.svg";
         answerBox[i].style = "display:block";
-    }
-    else {
-        haha = 'down';
+    } else {
+        toggleBtn[i] = 0;
         accoridion[i].src = "/images/chevron-down-solid.svg";
         answerBox[i].style = "display:none";
     }
-    
 }
-
