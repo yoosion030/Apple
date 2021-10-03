3415e4179c1a36dc960c9ea33272484a9df71222
yoosion <rsy5487@naver.com>
2021-10-03 10:29:25 +0900

    토글 제이쿼리 슬라이드 추가

--- src/support.js
+++ src/support.js
@@ -1,17 +1,23 @@
 const questionBox = document.querySelectorAll('.questionBox');
-const accoridion = document.querySelectorAll('.accordion');
+const accordion = document.querySelectorAll('.accordion');
 const answerBox = document.querySelectorAll('.answerBox');
-
-let toggleBtn = [0, 0];
+const toggleBtn = [0, 0, 0, 0, 0, 0, 0, 0];
 
 function answer(i) {
     if (toggleBtn[i] === 0) {
         toggleBtn[i] = 1;
-        accoridion[i].src = "/images/chevron-up-solid.svg";
-        answerBox[i].style = "display:block";
+        accordion[i].src = "/images/chevron-up-solid.svg";
+        $(answerBox[i]).slideDown(500,function(){
+            style = "display:block";
+        });
     } else {
         toggleBtn[i] = 0;
-        accoridion[i].src = "/images/chevron-down-solid.svg";
-        answerBox[i].style = "display:none";
+        accordion[i].src = "/images/chevron-down-solid.svg";
+        // setTimeout(() => {
+        //     accordion[i].src = "/images/chevron-down-solid.svg";
+        // }, 200);
+        $(answerBox[i]).slideUp(500,function(){
+            style = "display:none";
+        });
     }
 }
