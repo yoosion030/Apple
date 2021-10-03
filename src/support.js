051899790135af4db1f8c32c1205e08a6f6d12da
yoosion <rsy5487@naver.com>
2021-10-03 13:26:23 +0900

    이미지 경로 수정

--- src/support.js
+++ src/support.js
@@ -6,13 +6,13 @@ const toggleBtn = [0, 0, 0, 0, 0, 0, 0, 0];
 function answer(i) {
     if (toggleBtn[i] === 0) {
         toggleBtn[i] = 1;
-        accordion[i].src = "/images/chevron-up-solid.svg";
+        accordion[i].src = "./images/chevron-up-solid.svg";
         $(answerBox[i]).slideDown(400,function(){
             style = "display:block";
         });
     } else {
         toggleBtn[i] = 0;
-        accordion[i].src = "/images/chevron-down-solid.svg";
+        accordion[i].src = "./images/chevron-down-solid.svg";
         $(answerBox[i]).slideUp(400,function(){
             style = "display:none";
         });
